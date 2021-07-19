bb597e46a2fb267ba0ffcdf6d644b260b83b645d
bonces <bonces143@gmail.com>
2021-07-19 13:28:13 +0700

    pake try excep dan else

--- pegawai/views.py
+++ pegawai/views.py
@@ -102,7 +102,11 @@ def HitungPangkatView(request, id):
     try:
         pangkat = urllib.request.urlopen(urlpangkat + str(pegawai.id))
         json_pangkat = json.load(pangkat)
-        print(json_pangkat)
+    except SocketError as e:
+        if e.errno != errno.ECONNRESET:
+            raise # Not error we are looking for
+        # pass # Handle error here
+    else:
         for pkt in json_pangkat:
             list_pangkat = GolonganHistoryModel.objects.filter(pengguna=pegawai.id).update_or_create(
                 id=pkt['id'], 
@@ -111,10 +115,6 @@ def HitungPangkatView(request, id):
                 nip=pegawai.nip, jenis=pkt['jenis'], 
                 tanggal=pkt['date'],
                 nomor_sk = pkt['name'])
-    except SocketError as e:
-        if e.errno != errno.ECONNRESET:
-            raise # Not error we are looking for
-        pass # Handle error here
     return render(request, template_name,context)
 
 
